22893ca2d705f0da1eb1900e2bd747e9938b937c
DREAMWORLD <>
2023-12-26 18:04:33 +0530

    - Make method optional in ZLPhotoBrowserWrapper.

--- Sources/Extensions/Array+ZLPhotoBrowser.swift
+++ Sources/Extensions/Array+ZLPhotoBrowser.swift
@@ -28,7 +28,7 @@ import Photos
 import UIKit
 
 extension ZLPhotoBrowserWrapper where Base == [PHAsset] {
-    func removeDuplicate() -> [PHAsset] {
+    public func removeDuplicate() -> [PHAsset] {
         return base.enumerated().filter { index, value -> Bool in
             base.firstIndex(of: value) == index
         }.map { $0.element }
@@ -36,7 +36,7 @@ extension ZLPhotoBrowserWrapper where Base == [PHAsset] {
 }
 
 extension ZLPhotoBrowserWrapper where Base == [ZLResultModel] {
-    func removeDuplicate() -> [ZLResultModel] {
+    public func removeDuplicate() -> [ZLResultModel] {
         return base.enumerated().filter { index, value -> Bool in
             base.firstIndex(of: value) == index
         }.map { $0.element }
